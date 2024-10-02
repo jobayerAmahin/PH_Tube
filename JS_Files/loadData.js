56367fe699be1c7353c3d62b642eba590d822615
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-10-03 01:03:32 +0300

    Adding video api and load video data

--- JS_Files/loadData.js
+++ JS_Files/loadData.js
@@ -1,4 +1,4 @@
-//
+//Load Initial Data
 const loadData=()=>{
     try{
         fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
@@ -12,6 +12,33 @@ const loadData=()=>{
 
 const showCategory=(data)=>{
     console.log(data)
+    data.forEach(elem => {
+        console.log(elem)
+        const categBtn=document.createElement('button')
+        categBtn.innerText=elem.category
+
+        categBtn.classList.add('btn')
+        document.getElementById('fetchBtn').appendChild(categBtn)
+    });
 }
 
 loadData()
+
+//Load Video
+
+const loadVideo=()=>{
+    try{
+        fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+            .then(res=>res.json())
+            .then(data=>showVideo(data.videos))
+    }
+    catch(error){
+        console.log('Error found', error)
+    }
+}
+
+const showVideo=(loadedVideos)=>{
+    console.log(loadedVideos)
+}
+
+loadVideo()
